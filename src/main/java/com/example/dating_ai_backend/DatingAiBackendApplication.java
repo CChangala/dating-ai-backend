b7b9a96e6beac17610804f03ce9ac488bdc8f9da
tany0812 <chaitanyachangala8@gmail.com>
2025-02-24 20:06:33 -0500

    logic to create all profiles on db

--- src/main/java/com/example/dating_ai_backend/DatingAiBackendApplication.java
+++ src/main/java/com/example/dating_ai_backend/DatingAiBackendApplication.java
@@ -1,79 +1,25 @@
 package com.example.dating_ai_backend;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.example.dating_ai_backend.conversations.ChatMessage;
-import com.example.dating_ai_backend.conversations.Conversation;
-import com.example.dating_ai_backend.conversations.ConversationRepository;
-import com.example.dating_ai_backend.profiles.Gender;
-import com.example.dating_ai_backend.profiles.Profile;
-import com.example.dating_ai_backend.profiles.ProfileRepository;
+
+import com.example.dating_ai_backend.profiles.ProfileCreationService;
 
 
 @SpringBootApplication
 public class DatingAiBackendApplication  implements CommandLineRunner{
 
 	@Autowired
-	private ProfileRepository profileRepository;
-
-	@Autowired
-	private ConversationRepository conversationRepository;
-
-	@Autowired
-	private OllamaChatModel chatClient;
+	private ProfileCreationService profileCreationService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatingAiBackendApplication.class, args);
 	}
 
 	public void run(String... args){
-		Prompt prompt = new Prompt("Who is brad pitt??");
-		ChatResponse response = chatClient.call(prompt);
-		System.out.println(response.getResult().getOutput().getText());
-		profileRepository.deleteAll();
-		conversationRepository.deleteAll();
-		Profile profile = new Profile(
-			"1",
-			"Jane",
-			"Doe",
-			28,
-			"Asian",
-			Gender.FEMALE,
-			"Bio of Jane Doe",
-			"http://example.com/image.png",
-			"INTJ"
-		);
-
-		Profile profile2 = new Profile(
-			"2",
-			"Tany",
-			"Changala",
-			28,
-			"Asian",
-			Gender.FEMALE,
-			"Bio of tany",
-			"http://example.com/image.png",
-			"INTJ"
-		);
-
-		Conversation conversation = new Conversation("1", profile.id(), List.of(
-			new ChatMessage("Hii", profile.id(), LocalDateTime.now())
-		));
-		profileRepository.save(profile);
-		profileRepository.save(profile2);
-		profileRepository.findAll().forEach(System.out::println);
-
-		conversationRepository.save(conversation);
-
-		conversationRepository.findAll().forEach(System.out::println);
+		profileCreationService.saveProfilesToDB();
 	}
 
 }
--- src/main/java/com/example/dating_ai_backend/profiles/ProfileCreationService.java
+++ src/main/java/com/example/dating_ai_backend/profiles/ProfileCreationService.java
@@ -1,22 +1,56 @@
 package com.example.dating_ai_backend.profiles;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 @Service
 public class ProfileCreationService {
 
+    
     private ProfileRepository profileRepository;
+
+    @Value("#{${tinderai.character.user}}")
+    private Map<String, String> userProfileProperties;
+
+    private static final String PROFILES_FILE_PATH = "../../../../../../../profiles.json";
 
     public ProfileCreationService(ProfileRepository profileRepository){
         this.profileRepository = profileRepository;
     }
 
     public void saveProfilesToDB() {
-       
+        Gson gson = new Gson();
+        try {
+            List<Profile> profiles= gson.fromJson(PROFILES_FILE_PATH, new TypeToken<ArrayList<Profile>>() {}.getType());
+            if (profiles != null) {
+                // Delete all current profiles and save new ones from JSON
+                profileRepository.deleteAll();
+                profileRepository.saveAll(profiles);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        Profile profile = new Profile(
+                userProfileProperties.get("id"),
+                userProfileProperties.get("firstName"),
+                userProfileProperties.get("lastName"),
+                Integer.parseInt(userProfileProperties.get("age")),
+                userProfileProperties.get("ethnicity"),
+                Gender.valueOf(userProfileProperties.get("gender")),
+                userProfileProperties.get("bio"),
+                userProfileProperties.get("imageUrl"),
+                userProfileProperties.get("myersBriggsPersonalityType")
+        );
+        System.out.println(userProfileProperties);
+        profileRepository.save(profile);
     }
 }
